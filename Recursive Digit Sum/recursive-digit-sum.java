a9d0b6963a78ffb39ab3ee165c98ad7be7a2273a
İlker Tınkır <ilkertnkr@gmail.com>
2021-12-30 22:26:54 +0300

    https://www.hackerrank.com/challenges/recursive-digit-sum/problem

---
+++ Recursive Digit Sum/recursive-digit-sum.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+     
+     
+
+    public static int superDigit(String n, int k) {
+    // Write your code here
+        String first = n;
+        /*
+        for(int i = 0; i<k; i++){
+           first += n;
+        }*/
+        long superInt = 0;      
+        for(int i = 0; i < first.length(); i++){
+            superInt += Integer.parseInt("" + first.charAt(i));
+        }
+        superInt *= k;
+        
+        
+        if(superInt < 10){
+            return (int) superInt;
+        }else{  
+            return superDigit("" + superInt, 1);     
+        }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
